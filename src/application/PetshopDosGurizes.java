2d052c976805878a44ae0723bed23c9afbcf3068
Luan Henzo de Almeida <43920813+luansinh0@users.noreply.github.com>
2022-06-08 21:27:34 -0300

    Projeto iniciado.

---
+++ src/application/PetshopDosGurizes.java
@@ -0,0 +1,14 @@
+package application;
+
+import views.FrmMain;
+
+/**
+ *
+ * @author luansinh0
+ */
+public class PetshopDosGurizes {
+    public static void main(String[] args) {
+        new FrmMain().setVisible(true);
+    }
+    
+}
